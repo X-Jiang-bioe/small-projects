122d7bb2ab6ef39e559000795ed648a1c64160c3
xj1209 <xj1209@uw.edu>
2022-10-14 16:40:17 -0700

    protein motif problem

--- Rosalind/find_motif.py
+++ Rosalind/find_motif.py
@@ -1,15 +1,78 @@
 import re
+import parse_fasta as parse
+import requests
 
 
-def find_motif(motif, dna):
+def find_motif(motif, sequence):
     """
+    Solution for finding motif in DNA
+
     Returns list of locations (1-indexed!!!) where the motif was found
     """
-    p = re.finditer(f"(?={motif})", dna)
-    return [match.start()+1 for match in p]
+    p = re.finditer(f"(?={motif})", sequence)
+    return (match.start()+1 for match in p)
+
+
+def find_nglycosylation(ids):
+    '''
+    Solution for Finding Protein Motif problem
+
+    Finds N-Glycosilatiion motifs' locations (1-indexed)
+    within a protein string given the uniprot IDs of said proteins.
+
+    The fuction utilizes Stream endpoint which is taxing on
+    uniprot servers.
+    Therefore, Only **small** batches of ids are recommended
+
+    Retruns
+    -------
+    generator object with each element of the form:
+    ('id', (motif_loc1, motif_loc2,...))
+    '''
+    link_l = "https://rest.uniprot.org/uniprotkb/"
+    link_r = ".fasta"
+
+    motif_reg = "[N][^P][S,T][^P]"
+    idtrim_reg = ".*?(?=(_))"
+    # id_reg = "\\|(.*?)\\|"
+    fas = ""
+    with requests.Session() as s:
+        for id in ids:
+            match = re.search(idtrim_reg, id)
+            # If id has extraneous modifiers (XXXX_modifier_modiefier2)
+            if match:
+                # strip modifiers
+                id = match[0]
+            # put protein seq. in list
+            r = s.get(link_l + id + link_r)
+            fas += r.text
+
+    # process whole thing in a batch
+    strings = tuple(parse.parse_fasta_string(fas).values())
+
+    # for string, id in zip(strings, ids):
+    locs = \
+        ((id, tuple(find_motif(motif_reg, s))) for id, s in zip(ids, strings))
+
+    return locs
 
 
 if __name__ == '__main__':
-    f = open('files/rosalind_subs.txt')
+    # commented out the 'finding motif in DNA' problem stuff
+    # f = open('files/rosalind_subs.txt')
+    # lis = f.read().split('\n')
+    # print(*find_motif(lis[1], lis[0]))
+
+    # 'finding protein motif' problem below
+    test = """A2Z669
+B5ZC00
+P07204_TRBM_HUMAN
+P20840_SAG1_YEAST"""
+    # lis = test.split("\n")
+    f = open('files/rosalind_mprt.txt')
     lis = f.read().split('\n')
-    print(*find_motif(lis[1], lis[0]))
+
+    for pair in find_nglycosylation(lis):
+        if len(pair[1]) > 0:
+            print(pair[0])
+            print(*pair[1])
